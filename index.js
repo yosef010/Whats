8d39d04605d87d8b6eca646e3f14f94742a9f8d7
yosef010 <yosefyahia223@gmail.com>
2026-08-19 14:07:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const QRCode = require('qrcode');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 let latestQR = '';
 
 // صفحة لعرض الـ QR كصورة واضحة
